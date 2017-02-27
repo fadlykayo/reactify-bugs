e096970cdb5790756cd3b90b1b934130b293391e
Fadly Kayo <fadly.kayo@googlemail.com>
2017-02-27 20:51:58 +0700

    Edit eslint

--- bug-tracker-react/.eslintrc.js
+++ bug-tracker-react/.eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": [
-        "standard",
-        "promise"
-    ]
-};
+  'extends': 'standard',
+  'plugins': [
+    'react',
+    'jsx-a11y',
+    'import'
+  ],
+  'rules': {
+    //  'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error'
+  }
+}
